20742c9ea8878b0102102848ac071bb911081324
Jair-F <jair.fehlauer@gmail.com>
2025-05-31 15:41:08 +0000

    added send packet

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import wakeonlan
+from wakeonlan import send_magic_packet
+
+PC_MAC_ADDRESS = "04:7c:16:80:a5:71"
+
+if __name__ == "__main__":
+    print(F"sending WOL to {PC_MAC_ADDRESS}")
+    send_magic_packet(PC_MAC_ADDRESS)
