7f4c2c6e833ce43fcc627527c8a9d91d224d48a5
Jair-F <jair.fehlauer@gmail.com>
2025-05-31 16:31:51 +0000

    push of state

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from wakeonlan import send_magic_packet
+from flask import Flask, render_template
 
 PC_MAC_ADDRESS = "04:7c:16:80:a5:71"
 
-if __name__ == "__main__":
-    print(F"sending WOL to {PC_MAC_ADDRESS}")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/send_wol_packet')
+def send_wol():
     send_magic_packet(PC_MAC_ADDRESS)
+    print(F"sending WOL to {PC_MAC_ADDRESS}")
+    return F"sending WOL to {PC_MAC_ADDRESS}"
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', '80')
