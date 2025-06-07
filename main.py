efbc22dc67ca76e58519ce5115444d20630996bb
Jair-F <jair.fehlauer@gmail.com>
2025-06-07 18:13:57 +0000

    refactor db interface

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 from wakeonlan import send_magic_packet
 from flask import Flask, redirect, render_template, request, url_for
-import sqlite3
-import os
-import getmac
-import scapy
-import re
+from modules.db_interface import Persistant
 
 # SERVER_DIR_PATH = "/media/server/"
 SERVER_DIR_PATH = "./"
 DATABASE_FILENAME = "database.sqlite"
-DB_WAKEONLAN_TABLE = "WAKEONLAN"
 
 app = Flask(__name__)
+persistant = Persistant(SERVER_DIR_PATH + DATABASE_FILENAME)
 
-def is_mac_address_valid(mac_addr):
-    return re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac_addr.lower())
 
 @app.route('/')
 def index():
-    pc_mac_addresses = []
-    with sqlite3.connect(SERVER_DIR_PATH + DATABASE_FILENAME) as database:
-            cursor = database.cursor()
-            cursor.execute(F"SELECT pc_name, mac_addr FROM {DB_WAKEONLAN_TABLE}")
-            pc_mac_addresses = cursor.fetchall()
-            print("database query: ", pc_mac_addresses)
-            tmp = {}
-            for item in pc_mac_addresses:
-                tmp[item[0]] = item[1]
+    return render_template('index.html', pc_list=persistant.get_pc_mac_addresses())
 
-    return render_template('index.html', pc_list=pc_mac_addresses)
 
 @app.route('/add_pc', methods=['GET', 'POST'])
 def add_pc():
@@ -36,17 +21,11 @@ def add_pc():
         mac_addr = request.form['MAC_ADDR']
         pc_name = request.form['PC_NAME']
 
-        if not is_mac_address_valid(mac_addr) or len(pc_name.strip()) == 0:
+        if not persistant.add_pc_mac_addr(pc_name, mac_addr):
             return "mac address or pc name not valid"
 
-        print("request to send wol for: ", pc_name, " ", mac_addr)
-
-        with sqlite3.connect(SERVER_DIR_PATH + DATABASE_FILENAME) as database:
-            database.execute(F"INSERT INTO {DB_WAKEONLAN_TABLE} (pc_name, mac_addr) VALUES(?, ?)", (pc_name, mac_addr))
-            database.commit()
-
-        send_magic_packet(mac_addr)
     return render_template('add_pc.html')
+
 
 @app.route('/send_wol_packet', methods=['GET', 'POST'])
 def send():
@@ -57,21 +36,6 @@ def send():
         send_magic_packet(mac_addr)
     return redirect(url_for('index'))
 
-# @app.route('/send_wol_packet', methods=['GET'])
-# def send_wol():
-#     send_magic_packet(PC_MAC_ADDRESSES)
-#     print(F"sending WOL to {PC_MAC_ADDRESSES}")
-#     return F"sending WOL to {PC_MAC_ADDRESSES}"
 
-def run():
-    clean_starup = not os.path.exists(SERVER_DIR_PATH + DATABASE_FILENAME)
-    if clean_starup:
-        print("no database found - creating ", SERVER_DIR_PATH + DATABASE_FILENAME)
-
-    with sqlite3.connect(SERVER_DIR_PATH + DATABASE_FILENAME) as database:
-        database.execute(F"CREATE TABLE IF NOT EXISTS {DB_WAKEONLAN_TABLE} (id INTEGER PRIMARY KEY AUTOINCREMENT, pc_name TEXT, mac_addr TEXT);")
-        database.commit()
-
-        app.run(host='0.0.0.0', port='5001')
-
-run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5001')
---
+++ modules/db_interface.py
@@ -0,0 +1,45 @@
+import os
+import sqlite3
+import re
+
+def is_mac_address_valid(mac_addr):
+    return re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac_addr.lower())
+
+class Persistant:
+    def __init__(self, database_path):
+        self.DB_WAKEONLAN_TABLE = "WAKEONLAN"
+        self.database_path = database_path
+
+        clean_starup = not os.path.exists(self.database_path)
+        if clean_starup:
+            print("no database found - creating ", self.database_path)
+
+        with sqlite3.connect(self.database_path) as database:
+            database.execute(F"CREATE TABLE IF NOT EXISTS {self.DB_WAKEONLAN_TABLE} (id INTEGER PRIMARY KEY AUTOINCREMENT, pc_name TEXT, mac_addr TEXT);")
+            database.commit()
+    
+    def get_pc_mac_addresses(self) -> dict:
+        with sqlite3.connect(self.database_path) as database:
+            cursor = database.cursor()
+            cursor.execute(F"SELECT pc_name, mac_addr FROM {self.DB_WAKEONLAN_TABLE}")
+            pc_mac_addresses = cursor.fetchall()
+
+            tmp = {}
+            for item in pc_mac_addresses:
+                tmp[item[0]] = item[1]
+
+            return tmp
+
+    def add_pc_mac_addr(self, pc_name, mac_addr) -> bool:
+        if not is_mac_address_valid(mac_addr) or len(pc_name.strip()) == 0:
+            return False
+
+        with sqlite3.connect(self.database_path) as database:
+            database.execute(F"INSERT INTO {self.DB_WAKEONLAN_TABLE} (pc_name, mac_addr) VALUES(?, ?)", (pc_name, mac_addr))
+            database.commit()
+
+            print(F"saved {pc_name} - {mac_addr} to db")
+            return True
+    
+    def delete_pc_mac_addresses(self, pc_name, mac_address):
+        pass
